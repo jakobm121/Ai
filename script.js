0d1201239ddf4182390f58b1f02c24f4e73bc8a8
jakobm121 <magic.betting@gmail.com>
2026-05-04 23:31:32 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const siteNav = document.querySelector('.site-nav');
-if (navToggle && siteNav) {
-  navToggle.addEventListener('click', () => {
-    const open = siteNav.classList.toggle('open');
-    navToggle.setAttribute('aria-expanded', open ? 'true' : 'false');
-  });
-}
